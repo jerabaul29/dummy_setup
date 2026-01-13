d8eb89259783839b58a4139a9e3d5c7f4e276585
JR <jerabaul29@users.noreply.github.com>
2026-01-13 14:04:53 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -15,7 +15,7 @@ echo "--- nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 # sourcing is the usual way to do it, but does not work within the script (probably because of bashrc interactive session check)
 # source ~/.bashrc
-# load npm explicitly!
+# load nvm explicitly!
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 echo "--- node"
