d734bcac45f3cebfc66056e0047f88afe0dbcf1a
JR <jerabaul29@users.noreply.github.com>
2026-01-13 13:37:32 +0100

    Implement dummy setup script with installations
    Added a dummy setup script that installs nvm, node, and Copilot CLI.

--- main.sh
+++ main.sh
@@ -1,5 +1,28 @@
-echo "Hello world"
-sleep 600
-mkdir -p $HOME/some_location
-touch $HOME/some_location/myfile
-echo "writing" >> $HOME/some_location/myfile
+echo "Dummy setup start..."
+echo ""
+
+# show basic stats
+echo "date:"
+date
+echo ""
+
+echo "pwd:"
+pwd
+echo ""
+
+echo "install nvm, node, copilot cli"
+echo "--- nvm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+source ~/.bashrc
+echo "--- node"
+nvm install node
+echo "--- copilot cli"
+npm install -g @github/copilot
+echo ""
+
+# if I wanted to create a folder, add a file, write to it, I would for example:
+#   mkdir -p $HOME/some_location
+#   touch $HOME/some_location/myfile
+#   echo "writing" >> $HOME/some_location/myfile
+
+echo "... dummy setup end"
