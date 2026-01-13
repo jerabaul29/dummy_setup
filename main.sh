3237d74698ec72b7b0269cc845cc841a7e141341
JR <jerabaul29@users.noreply.github.com>
2026-01-13 14:04:22 +0100

    Add comments for nvm and npm loading process
    Added comments explaining the sourcing of .bashrc and loading npm.

--- main.sh
+++ main.sh
@@ -13,7 +13,11 @@ echo ""
 echo "install nvm, node, copilot cli"
 echo "--- nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-source ~/.bashrc
+# sourcing is the usual way to do it, but does not work within the script (probably because of bashrc interactive session check)
+# source ~/.bashrc
+# load npm explicitly!
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 echo "--- node"
 nvm install node
 echo "--- copilot cli"
